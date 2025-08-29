cc8cc0c5d49d2379c1f7b426be1da9399270ddbb
Peter Speak <Peterspeaking@gmail.com>
2025-08-29 12:31:31 -0500

    Adding completion of tasks

--- todo.py
+++ todo.py
@@ -18,18 +18,16 @@ def delete_tasks(task):
     print(f"Deleted: {task}")
 
 # Step 5: Mark task complete
+def mark_complete(index):
+    if 0 <= index < len(tasks):
+        tasks[index] = tasks[index] + " ✅"
 
-
-# Step 6: Save/load tasks (extra stretch for today)
-
-
-# Demo flow (you can run this file directly: python todo.py)
 if __name__ == "__main__":
     add_task("Finish Cyber 201 assignment")
     add_task("Push code to GitHub")
     view_tasks()
     delete_tasks(0)
     view_tasks()
-    #mark_complete(0)
-    #view_tasks()
+    mark_complete(0)
+    view_tasks()
     #save_tasks()
