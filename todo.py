84921edd00cfbf17e0b5a22f5cb710180ae64678
Peter Speak <Peterspeaking@gmail.com>
2025-09-04 10:36:35 -0500

    Delete Works! Woohoo!

--- todo.py
+++ todo.py
@@ -13,18 +13,19 @@ def view_tasks():
         print(f"{i}. {task}")
 
 # Step 4: Delete a task
-
+def delete_tasks(index):
+    task = tasks.pop(index)
+    print(f"Deleted: {task}")
 
 # Step 5: Mark task complete
-
-
-# Step 6: Save/load tasks (extra stretch for today)
 
 
 # Demo flow (you can run this file directly: python todo.py)
 if __name__ == "__main__":
     add_task("Finish Cyber 201 assignment")
     add_task("Push code to GitHub")
+    view_tasks()
+    delete_tasks(0)
     view_tasks()
     # mark_complete(0)
     # view_tasks()
